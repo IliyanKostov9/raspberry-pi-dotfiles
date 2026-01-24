e67fd58f52d20b751a211a59d19967f44e83d250
PI <pi@pi.com>
2026-01-24 15:16:46 +0000

    fix: immich

--- shells/ubuntu/dev.sh
+++ shells/ubuntu/dev.sh
@@ -3,11 +3,4 @@ git clone https://github.com/rafi/awesome-vim-colorschemes.git ~/temp/
 mkdir ~/.vim
 mv ~/temp/colors/ ~/.vim/
 rm -rf ~/temp
-
-echo "Now installing zsh..."
-sudo apt install -y zsh
-sudo chsh -s $(which zsh)
-sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-sudo mv /root/.oh-my-zsh ~/
 source ~/.zshrc
